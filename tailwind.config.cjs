240ee71fcdab2e1957e45e45c9de854eead25a5d
Wojtek Malarski <w.malarski@gmail.com>
2025-03-03 23:15:49 +0100

    tailwind migration started

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,4 @@
-const cyberpunkTheme = require("daisyui/src/theming/themes").cyberpunk;
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  daisyui: {
-    themes: [
-      {
-        "cyberpunk-dark": {
-          ...cyberpunkTheme,
-          "base-100": "#1c1917",
-          "base-200": "#292524",
-          "base-300": "#57534e",
-          "base-content": "#e7e5e4",
-          "color-scheme": "dark",
-          neutral: "#e7e5e4",
-          "neutral-content": "#191D24",
-          "neutral-focus": "#f8f8f8",
-        },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
 };
